77e434e0845963d62e4e9d1b82006d834f139e45
Alexander Carter <alrunner4@gmail.com>
2026-06-20 15:15:43 -0700

    Antigravity builds a WebTorrent gallery

--- Makefile
+++ Makefile
@@ -14,3 +14,21 @@ resume-latex.pdf: resume.html
 
 resume-htmldoc.pdf: resume.html
 	nix-shell -p htmldoc --run 'htmldoc $< -f $@'
+
+# Gallery Build Targets
+.PHONY: build-gallery clean-gallery serve
+
+build-gallery:
+	nix-build
+	cp -f result/gallery.torrent ./
+	rm -f result
+	@echo "Build successful! Created gallery.torrent from gallery-media."
+
+clean-gallery:
+	rm -f gallery.torrent
+	rm -f result
+
+serve:
+	@echo "Starting local HTTP development server at http://localhost:8000"
+	@echo "Press Ctrl+C to stop."
+	nix-shell -p python3 --run "python3 -m http.server 8000"
---
+++ default.nix
@@ -0,0 +1,39 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+pkgs.stdenv.mkDerivation {
+  pname = "aetherstream-gallery";
+  version = "1.0.0";
+
+  # Include the files in the directory as source
+  src = ./.;
+
+  # Include mktorrent as a build dependency
+  nativeBuildInputs = [ pkgs.mktorrent ];
+
+  # Build phase: generate the .torrent file from gallery-media folder
+  buildPhase = ''
+    echo "Creating torrent from gallery-media folder..."
+    
+    # We specify WebTorrent WebRTC trackers using -a
+    # -a: wss://tracker.openwebtorrent.com
+    # -a: wss://tracker.btorrent.xyz
+    # We also embed the GitHub Pages production URL as a Web Seed using -w
+    # Remove existing torrent file in the build tree if it exists to prevent mktorrent from failing
+    rm -f gallery.torrent
+
+    mktorrent -v \
+      -a wss://tracker.openwebtorrent.com,wss://tracker.btorrent.xyz \
+      -w https://alrunner4.github.io/ \
+      -o gallery.torrent \
+      gallery-media
+  '';
+
+  # Install phase: bundle the client html and torrent file for deployment
+  installPhase = ''
+    mkdir -p $out
+    cp index.html $out/index.html
+    cp gallery.torrent $out/gallery.torrent
+    
+    echo "Build completed. Artifacts index.html and gallery.torrent are in $out."
+  '';
+}
